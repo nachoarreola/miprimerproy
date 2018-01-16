8dd2f40cdc5f6bca56985ca3d18e6c2c55a4e7d2
macbookAir <cristian@encalient.es>
2018-01-16 16:00:20 -0600

    Initial Commit

---
+++ MiPrimerProyecto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiPrimerProyecto
+//
+//  Created by Nacho Arreola on 1/16/18.
+//  Copyright © 2018 Nacho Arreola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
